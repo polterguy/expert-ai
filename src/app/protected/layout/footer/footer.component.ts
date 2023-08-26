abc4b9985efc3eed24651927f1c9cbbad99a403a
Thomas Hansen <th@aista.com>
2023-08-26 10:25:49 +0300

    Micro improvements

--- src/app/protected/layout/footer/footer.component.ts
+++ src/app/protected/layout/footer/footer.component.ts
@@ -3,7 +3,7 @@
  * Copyright (c) Arta-Marketing AS, 2023 team@ainiro.io, all rights reserved.
  */
 
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 /**
  * Footer component for displaying footer and copyright information.
@@ -13,14 +13,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
-
-  currentYear: number = 0;
+export class FooterComponent {
 
   constructor() { }
-
-  ngOnInit() {
-
-    this.currentYear = new Date().getFullYear();
-  }
 }
